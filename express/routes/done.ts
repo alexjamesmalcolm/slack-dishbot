245faff5246c67aae34b47a525faf923b52978f5
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2021-03-11 02:11:26 -0500

    Finished done command

--- express/routes/done.ts
+++ express/routes/done.ts
@@ -1,13 +1,61 @@
 import { RequestHandler } from "express";
 import { connect } from "../../mongodb";
-import Dishwheel from "../../types/dishwheel";
+import Dishwheel, { getNextDishwasher } from "../../types/dishwheel";
 import SlashMessage from "../../types/slash-message";
 
 export const done: RequestHandler = async (req, res) => {
-  const message = req.body as SlashMessage;
-  res.send();
-  fetch(message.response_url, {
-    method: "POST",
-    body: JSON.stringify({ text: "Finished" }),
-  });
+  const {
+    channel_id,
+    user_name: person,
+    channel_name,
+  } = req.body as SlashMessage;
+  const [mongo, close] = await connect();
+  const collectionOfDishwheels = mongo.collection<Dishwheel>("dishwheels");
+  const dishwheel = await collectionOfDishwheels.findOne(
+    { channel_id },
+    { timeout: true }
+  );
+  if (!dishwheel) {
+    res.send(`No dishwheel in channel ${channel_name}.`);
+  } else if (!dishwheel.dishwashers.includes(person)) {
+    res.send(`${person} is not a member of this channel's dishwheel.`);
+  } else if (dishwheel.currentDishwasher !== person) {
+    res.send(`${person} is not on dishes.`);
+  } else {
+    const alteredDishwheel: Dishwheel = {
+      ...dishwheel,
+      dateCurrentDishwasherStarted: new Date().toISOString(),
+      currentDishwasher: getNextDishwasher(dishwheel),
+    };
+    await collectionOfDishwheels.updateOne(
+      { channel_id },
+      {
+        $set: alteredDishwheel,
+      }
+    );
+    const isItPossibleForThereToBeAFine =
+      dishwheel.fineAmount > 0 && dishwheel.secondsUntilFine > 0;
+    const millisecondsOnDishes =
+      new Date().getTime() -
+      new Date(dishwheel.dateCurrentDishwasherStarted).getTime();
+    const countOfFinePeriodsPassed =
+      dishwheel.finePeriodicity > 0
+        ? 1 +
+          (millisecondsOnDishes - dishwheel.secondsUntilFine * 1000) /
+            (dishwheel.finePeriodicity * 1000)
+        : millisecondsOnDishes / (dishwheel.secondsUntilFine * 1000);
+    const accruedFine =
+      Math.floor(countOfFinePeriodsPassed) * dishwheel.fineAmount;
+    const hasFine = isItPossibleForThereToBeAFine && accruedFine > 0;
+    if (hasFine) {
+      res.send(
+        `${person} completed the dishes with a fine of $${accruedFine}, ${alteredDishwheel.currentDishwasher} is now up.`
+      );
+    } else {
+      res.send(
+        `${person} completed the dishes, ${alteredDishwheel.currentDishwasher} is now up.`
+      );
+    }
+  }
+  close();
 };
