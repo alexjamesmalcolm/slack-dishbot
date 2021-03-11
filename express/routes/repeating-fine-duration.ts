4fe25c936584ed5d59c1b66407b1132b984bec7f
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2021-03-11 02:22:40 -0500

    Started next routes

---
+++ express/routes/repeating-fine-duration.ts
@@ -0,0 +1,17 @@
+import { RequestHandler } from "express";
+import { connect } from "../../mongodb";
+import Dishwheel from "../../types/dishwheel";
+import SlashMessage from "../../types/slash-message";
+import { duration } from "moment";
+
+export const repeatingFineDuration: RequestHandler = async (req, res) => {
+  const { channel_id, channel_name } = req.body as SlashMessage;
+  const [mongo, close] = await connect();
+  const collectionOfDishwheels = mongo.collection<Dishwheel>("dishwheels");
+  const dishwheel = await collectionOfDishwheels.findOne(
+    { channel_id },
+    { timeout: true }
+  );
+  // TODO
+  close();
+};
