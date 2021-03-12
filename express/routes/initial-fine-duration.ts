d5fc8027ddf5c0f2a01076d28b09d2962960cade
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2021-03-11 22:03:47 -0500

    Finished inital-fine-duration command

--- express/routes/initial-fine-duration.ts
+++ express/routes/initial-fine-duration.ts
@@ -2,16 +2,52 @@ import { RequestHandler } from "express";
 import { connect } from "../../mongodb";
 import Dishwheel from "../../types/dishwheel";
 import SlashMessage from "../../types/slash-message";
-import { duration } from "moment";
 
 export const initialFineDuration: RequestHandler = async (req, res) => {
-  const { channel_id, channel_name } = req.body as SlashMessage;
+  const {
+    channel_id,
+    channel_name,
+    text,
+    user_id,
+    user_name,
+  } = req.body as SlashMessage;
   const [mongo, close] = await connect();
   const collectionOfDishwheels = mongo.collection<Dishwheel>("dishwheels");
   const dishwheel = await collectionOfDishwheels.findOne(
     { channel_id },
     { timeout: true }
   );
-  // TODO
+  if (!dishwheel) {
+    res.send(`No dishwheel in channel ${channel_name}.`);
+  } else if (text.trim() === "") {
+    res.send(
+      `The initial fine duration in seconds is ${dishwheel.secondsUntilFine}.`
+    );
+  } else if (user_id !== dishwheel.creatorId) {
+    res.send(
+      `${user_name} cannot change the dishwheel's inital fine duration, only the creator of the dishwheel can.`
+    );
+  } else {
+    const secondsUntilFine = Number.parseFloat(text.trim());
+    if (!Number.isNaN(secondsUntilFine) && secondsUntilFine >= 0) {
+      const alteredDishwheel: Dishwheel = {
+        ...dishwheel,
+        secondsUntilFine,
+      };
+      await collectionOfDishwheels.updateOne(
+        { channel_id },
+        {
+          $set: alteredDishwheel,
+        }
+      );
+      res.send(
+        `Updated inital fine duration to ${dishwheel.secondsUntilFine} seconds.`
+      );
+    } else {
+      res.send(
+        `Could not set "${text}" as the inital fine duration in seconds.`
+      );
+    }
+  }
   close();
 };
