b64a8e6513ccb38e675f2c508936da8dbaca3fc4
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2021-03-11 21:51:16 -0500

    Created fine-amount command

--- express/routes/fine-amount.ts
+++ express/routes/fine-amount.ts
@@ -2,16 +2,46 @@ import { RequestHandler } from "express";
 import { connect } from "../../mongodb";
 import Dishwheel from "../../types/dishwheel";
 import SlashMessage from "../../types/slash-message";
-import { duration } from "moment";
 
 export const fineAmount: RequestHandler = async (req, res) => {
-  const { channel_id, channel_name } = req.body as SlashMessage;
+  const {
+    channel_id,
+    channel_name,
+    text,
+    user_id,
+    user_name,
+  } = req.body as SlashMessage;
   const [mongo, close] = await connect();
   const collectionOfDishwheels = mongo.collection<Dishwheel>("dishwheels");
   const dishwheel = await collectionOfDishwheels.findOne(
     { channel_id },
     { timeout: true }
   );
-  // TODO
+  if (!dishwheel) {
+    res.send(`No dishwheel in channel ${channel_name}.`);
+  } else if (text.trim() === "") {
+    res.send(`The fine amount is ${dishwheel.fineAmount}.`);
+  } else if (user_id !== dishwheel.creatorId) {
+    res.send(
+      `${user_name} cannot change the dishwheel's fine amount, only the creator of the dishwheel can.`
+    );
+  } else {
+    const fineAmount = Number.parseFloat(text.trim());
+    if (!Number.isNaN(fineAmount) && fineAmount > 0) {
+      const alteredDishwheel: Dishwheel = {
+        ...dishwheel,
+        fineAmount,
+      };
+      await collectionOfDishwheels.updateOne(
+        { channel_id },
+        {
+          $set: alteredDishwheel,
+        }
+      );
+      res.send(`Updated fine amount to ${dishwheel.fineAmount}.`);
+    } else {
+      res.send(`Could not set "${text}" as the fine amount.`);
+    }
+  }
   close();
 };
