3183fb754d9e3937cc91a66a1c7e504fd42233b6
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2021-06-06 15:05:39 -0400

    Created skip command

--- express/routes/skip.ts
+++ express/routes/skip.ts
@@ -1,3 +1,43 @@
 import { RequestHandler } from "express";
+import { connect } from "../../mongodb";
+import Dishwheel, { getNextDishwasher } from "../../types/dishwheel";
+import SlashMessage from "../../types/slash-message";
+import { respond } from "../respond";
+import { noDishwheelFoundResponse } from "./responses/no-dishwheel-found";
 
-export const skip: RequestHandler = () => {};
+export const skip: RequestHandler = async (req, res) => {
+  const message = req.body as SlashMessage;
+  const { channel_id, text, user_id, user_name, response_url } = message;
+  res.send();
+  const [mongo, close] = await connect();
+  const collectionOfDishwheels = mongo.collection<Dishwheel>("dishwheels");
+  const dishwheel = await collectionOfDishwheels.findOne(
+    { channel_id },
+    { timeout: true }
+  );
+  if (!dishwheel) {
+    noDishwheelFoundResponse(message);
+  } else if (user_id !== dishwheel.creatorId) {
+    respond(
+      response_url,
+      `${user_name} cannot skip ${dishwheel.currentDishwasher}, only the creator of the dishwheel can.`
+    );
+  } else {
+    const alteredDishwheel: Dishwheel = {
+      ...dishwheel,
+      dateCurrentDishwasherStarted: new Date().toISOString(),
+      currentDishwasher: getNextDishwasher(dishwheel),
+    };
+    await collectionOfDishwheels.updateOne(
+      { channel_id },
+      {
+        $set: alteredDishwheel,
+      }
+    );
+    respond(
+      response_url,
+      `${dishwheel.currentDishwasher} was skipped, ${alteredDishwheel.currentDishwasher} is now up.`
+    );
+  }
+  close();
+};
