358fbfad1351f42dcc72eb66139995c33d436a6f
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2021-11-03 16:06:54 -0400

    Modified humanizeDuration so it can produce as little as 1 and as many as 4 components

--- express/utils/humanizeDuration.test.ts
+++ express/utils/humanizeDuration.test.ts
@@ -26,4 +26,15 @@ describe("humanizeDuration", () => {
     const result = humanizeDuration(duration, true);
     expect(result).toBe("in 6 hours and 2 minutes");
   });
+  it("should display 5 hours, 62 minutes, and 5 seconds as 6 hours", () => {
+    const duration = Temporal.Duration.from({
+      hours: 5,
+      minutes: 62,
+      seconds: 5,
+    });
+
+    const result = humanizeDuration(duration, true, 1);
+
+    expect(result).toBe("in 6 hours");
+  });
 });
